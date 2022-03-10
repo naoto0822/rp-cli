0e83afc47389c2b1fd6f4aa3a2f4007b621ef042
naoto0822 <n.h.in.m.h@gmail.com>
2022-03-11 08:45:47 +0900

    added api.rs

---
+++ src/api.rs
@@ -0,0 +1,66 @@
+use crate::types::{execute, compile};
+
+use serde::{
+    Serialize,
+    de::DeserializeOwned,
+    Deserialize
+};
+
+const BASE_URL: &str = "https://play.rust-lang.org/";
+const PATH_EXECUTE: &str = "execute";
+const PATH_COMPILE: &str = "compile";
+
+pub trait API {
+    fn execute(&self, req: execute::Request) -> Result<execute::Response, Box<dyn std::error::Error>>;
+    fn compile(&self, req: compile::Request) -> Result<compile::Response, Box<dyn std::error::Error>>;
+}
+
+pub struct APIClient {
+    base_url: String
+}
+
+impl APIClient {
+    pub fn new() -> APIClient {
+        APIClient{
+            base_url: BASE_URL.to_string(),
+        }
+    }
+
+    fn post<S, D>(&self, path: &str, body: S) -> Result<D, Box<dyn std::error::Error>> where S: Serialize, D: DeserializeOwned {
+
+        let url = self.base_url.clone() + path;
+
+        let client = reqwest::blocking::Client::new();
+        let res: D = client
+            .post(url)
+            .json(&body)
+            .send()?
+            .json()?;
+
+        Ok(res)
+    }
+}
+
+impl API for APIClient  {
+
+    fn execute(&self, req: execute::Request) -> Result<execute::Response, Box<dyn std::error::Error>> {
+        let res: execute::Response = self.post(PATH_EXECUTE, req)?;
+        Ok(res)
+
+       // let url = self.base_url.clone() + "execute";
+
+       // let client = reqwest::blocking::Client::new();
+       // let res: Response = client
+       //     .post(url)
+       //     .json(&req)
+       //     .send()?
+       //     .json()?;
+
+       // Ok(res)
+    }
+
+    fn compile(&self, req: compile::Request) -> Result<compile::Response, Box<dyn std::error::Error>> {
+        let res: compile::Response = self.post(PATH_COMPILE, req)?;
+        Ok(res)
+    }
+}
