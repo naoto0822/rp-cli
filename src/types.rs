b2346d881a28c48cf8a7d58fd7652f55a01fcf80
naoto0822 <n.h.in.m.h@gmail.com>
2022-03-15 08:46:28 +0900

    decleard module

--- src/types.rs
+++ src/types.rs
@@ -1,25 +1,62 @@
-use serde::{Serialize, Deserialize};
+pub mod execute {
+    use serde::{Serialize, Deserialize};
 
-#[derive(Serialize)]
-pub struct Request {
-    #[serde(rename(serialize = "crateType"))]
-    pub crate_type: String,
-    pub tests: bool,
-    pub mode: String,
-    pub channel: String,
-    pub edition: String,
-    pub backtrace: bool,
-    pub code: String,
+    #[derive(Serialize)]
+    pub struct Request {
+        #[serde(rename(serialize = "crateType"))]
+        pub crate_type: String,
+        pub tests: bool,
+        pub mode: String,
+        pub channel: String,
+        pub edition: String,
+        pub backtrace: bool,
+        pub code: String,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Response {
+        #[serde(default)]
+        pub success: bool,
+        #[serde(default)]
+        pub stdout: String,
+        #[serde(default)]
+        pub stderr: String,
+        #[serde(default)]
+        pub error: String,
+    }
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Response {
-    #[serde(default)]
-    success: bool,
-    #[serde(default)]
-    stdout: String,
-    #[serde(default)]
-    stderr: String,
-    #[serde(default)]
-    error: String,
+pub mod compile {
+    use serde::{Serialize, Deserialize};
+
+    #[derive(Serialize)]
+    pub struct Request {
+        #[serde(rename(serialize = "crateType"))]
+        pub crate_type: String,
+        pub tests: bool,
+        pub mode: String,
+        pub channel: String,
+        pub edition: String,
+        pub backtrace: bool,
+        pub code: String,
+        #[serde(rename(serialize = "assemblyFlavor"))]
+        pub assembly_flavor: String,
+        #[serde(rename(serialize = "demangleAssembly"))]
+        pub demangle_assembly: String,
+        #[serde(rename(serialize = "processAssembly"))]
+        pub process_assembly: String,
+        pub target: String,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct Response {
+        #[serde(default)]
+        pub success: bool,
+        #[serde(default)]
+        pub stdout: String,
+        #[serde(default)]
+        pub stderr: String,
+        #[serde(default)]
+        pub error: String,
+    }
 }
